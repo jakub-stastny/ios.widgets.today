92d13bfb2c2b9246f2c4d0a7c81ad885a4a5df48
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2021-06-04 13:23:50 +0000

    SwiftOrg code from their README

--- Sources/ios.widgets.today/main.swift
+++ Sources/ios.widgets.today/main.swift
@@ -1,1 +1,11 @@
+import SwiftOrg
+
+let lines = [
+    "* TODO head line",
+    "  A normal line here.",
+]
+
+let parser = OrgParser()
+let doc = try parser.parse(lines: lines)
+
 print("Hello, world!")
